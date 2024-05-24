41eae8b20ea3386aee2ca3972a58f145089aa71f
nguyenvukhang <brew4k@gmail.com>
2024-05-24 13:33:34 +0800

    pool with pairs

--- run.py
+++ run.py
@@ -32,28 +32,34 @@ def download(url: str):
         raise RuntimeError("Could not download file")
 
 
-def full_run(fn: str):
-    print("start download...")
-    download(f"https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/{fn}")
+def full_run(p: tuple[str, str]):
+    fn, member = p
+    # print("start download...")
+    # download(f"https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/{fn}")
     with zipfile.ZipFile(f"output/.cache/{fn}", "r") as f:
-        for member in f.namelist():
-            if member.startswith("__MACOSX") or not member.endswith(".csv"):
-                continue
-            csvfile = path.join("output", member)
-            f.extract(member, path="output")
-            print("unzip:", csvfile)
-            tbl = pa.Table.from_pandas(pd.read_csv(csvfile))
-            pq.write_table(tbl, path.join("output", path.basename(member) + ".parquet"))
-            os.remove(csvfile)
+        csvfile = path.join("output", member)
+        f.extract(member, path="output")
+        print("unzip:", csvfile)
+        tbl = pa.Table.from_pandas(pd.read_csv(csvfile))
+        pq.write_table(tbl, path.join("output", path.basename(member) + ".parquet"))
+        os.remove(csvfile)
 
 pool = ThreadPool(processes=8)
 
-zips = []
+pairs = []
+
 for y in range(2018, 2025):
     for q in range(1, 5):
         if y == 2024 and q != 1:
             continue
-        zips.append(f"data_Q{q}_{y}.zip")
+        fn = f"data_Q{q}_{y}.zip"
+        with zipfile.ZipFile(f"output/.cache/{fn}", "r") as f:
+            for member in f.namelist():
+                if member.startswith("__MACOSX") or not member.endswith(".csv"):
+                    continue
+                pairs.append((fn, member))
+            print(fn, member)
 
 with ThreadPool(8) as p:
-    tqdm(pool.map(full_run, zips), total=len(zips))
+    total = len(pairs)
+    list(tqdm(pool.imap(full_run, pairs), total=total))
