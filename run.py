f280277b83d74a6f4672dd25be8e7e086708da05
nguyenvukhang <brew4k@gmail.com>
2024-05-24 13:01:10 +0800

    ok

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
-import csv, zipfile
+import zipfile, os
 from os import path
 import requests
 import pandas as pd
 import pyarrow.parquet as pq
 import pyarrow as pa
-import subprocess
 
 
 def rw():
@@ -48,9 +47,15 @@ def full_run(fn: str):
     print("start download...")
     download(f"https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/{fn}")
     with zipfile.ZipFile(f"output/.cache/{fn}", "r") as f:
-        for member in filter(lambda v: not v.startswith("__MACOSX"), f.namelist()):
-            f.extract(member, path="dank.csv")
-            print("unzip:", member)
+        for member in f.namelist():
+            if member.startswith("__MACOSX") or not member.endswith(".csv"):
+                continue
+            csvfile = path.join("output", member)
+            f.extract(member, path="output")
+            print("unzip:", csvfile)
+            tbl = pa.Table.from_pandas(pd.read_csv(csvfile))
+            pq.write_table(tbl, path.join("output", path.basename(member) + ".parquet"))
+            os.remove(csvfile)
 
 
 for y in range(2016, 2025):
