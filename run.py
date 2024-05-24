45a2fb686da265dcd1d2c0f9e6ced6f5adff226b
nguyenvukhang <brew4k@gmail.com>
2024-05-24 10:45:09 +0800

    try to manip

--- run.py
+++ run.py
@@ -1,6 +1,32 @@
 import csv
+import pandas as pd
+import pyarrow.parquet as pq
+import pyarrow as pa
 
-with open("2024-01-01.csv", "r") as f:
-    it = csv.reader(f)
-    print(next(it))
-    print(next(it))
+# pq.read_table
+# pd.DataFrame.from_
+
+
+def rw():
+    print("reading...")
+    df = pd.read_csv("2024-01-01.csv")
+    tbl = pa.Table.from_pandas(df)
+    print("read done.")
+    pq.write_table(tbl, 'example.parquet', write_page_index=False)
+
+    print("writing...")
+    df = pq.read_table("example.parquet").to_pandas()
+    df.to_csv("dank.csv")
+
+# rw()
+
+df1 = pd.read_csv("2024-01-01.csv")
+# print(df1)
+
+df2 = pq.read_table("example.parquet").to_pandas()
+# print(df2)
+
+s1 = pd.DataFrame(df1, columns=['smart_100_raw'])
+s2 = pd.DataFrame(df2, columns=['smart_100_raw'])
+print(s1[1:10])
+print(s2[1:10])
