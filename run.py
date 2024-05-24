d7e25d5fcc1cd16ff2fea4aa94d9f75ab524cea8
nguyenvukhang <brew4k@gmail.com>
2024-05-24 13:21:43 +0800

    Multiprocessing Threadpool

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import pandas as pd
 import pyarrow.parquet as pq
 import pyarrow as pa
 import concurrent.futures
+from multiprocessing.pool import ThreadPool
 
 
 def download(url: str):
@@ -46,14 +47,15 @@ def full_run(fn: str):
             pq.write_table(tbl, path.join("output", path.basename(member) + ".parquet"))
             os.remove(csvfile)
 
+pool = ThreadPool(processes=8)
 
-pool = concurrent.futures.ThreadPoolExecutor(max_workers=8)
+zips = []
 for y in range(2018, 2025):
     for q in range(1, 5):
         if y == 2024 and q != 1:
             continue
-        fn = f"data_Q{q}_{y}.zip"
-        pool.submit(lambda: full_run(fn))
+        zips.append(f"data_Q{q}_{y}.zip")
 
-pool.shutdown(wait=True)
+pool.map(full_run, zips)
+
 print("Main thread sending it")
