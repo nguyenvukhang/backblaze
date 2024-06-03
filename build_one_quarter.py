ade16eda731d4d6ea402a796bdb899896cd64a08
nguyenvukhang <brew4k@gmail.com>
2024-06-03 19:22:13 +0800

    Remove index column

--- build_one_quarter.py
+++ build_one_quarter.py
@@ -1,26 +1,15 @@
-import sys, requests, shutil
 from os import path
 import pandas as pd
 import pyarrow.parquet as pq
 import pyarrow as pa
 from zipfile import ZipFile
-import os
+import os, sys
 
 if len(sys.argv) < 2:
     print("Please supply download url as first CLI arg.", sys.argv)
     exit(1)
 
 url = sys.argv[1]
-
-
-def download_file(url) -> str:
-    local_filename = path.basename(url)
-    with requests.get(url, stream=True) as r:
-        if r.status_code == 404:
-            return None
-        with open(local_filename, "wb") as f:
-            shutil.copyfileobj(r.raw, f)
-    return local_filename
 
 
 fp = path.basename(url)
@@ -33,12 +22,13 @@ with ZipFile(fp, "r") as z:
         if member.startswith("__MACOSX") or not member.endswith(".csv"):
             continue
         members.append(member)
+print(members)
 
 for member in members:
     print("member:", member)
     with ZipFile(fp, "r") as z:
         z.extract(member)
-        df = pd.read_csv(member, delimiter=",", index_col=0)
+        df = pd.read_csv(member, delimiter=",")
         os.remove(member)
         pq.write_table(
             pa.Table.from_pandas(df),
