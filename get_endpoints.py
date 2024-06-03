19fa06693c1efc4394ff02ef3c9a27690d92a9d0
nguyenvukhang <brew4k@gmail.com>
2024-06-03 10:16:27 +0800

    Build all

--- get_endpoints.py
+++ get_endpoints.py
@@ -1,25 +1,11 @@
 from datetime import datetime
 from multiprocessing.pool import ThreadPool
-import requests, shutil, json
-from os import path
+import requests, json
 
 
 def dl_url(year: int, quarter: int):
     return f"https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q{quarter}_{year}.zip"
 
-
-def download_file(url) -> str:
-    local_filename = path.basename(url)
-    with requests.get(url, stream=True) as r:
-        if r.status_code == 404:
-            return None
-        with open(local_filename, "wb") as f:
-            shutil.copyfileobj(r.raw, f)
-
-    return local_filename
-
-
-datetime.today()
 
 potential_urls = []
 for year in range(2016, datetime.today().year + 1):
@@ -28,17 +14,14 @@ for year in range(2016, datetime.today().year + 1):
 
 
 def check_url(urls: list[str], idx: int):
-    url = urls[idx]
-    with requests.get(url, stream=True) as r:
+    with requests.get(urls[idx], stream=True) as r:
         if r.status_code != 200:
             urls[idx] = None
 
 
 with ThreadPool(4) as pool:
-    total = len(potential_urls)
     jobs = [(potential_urls, i) for i in range(len(potential_urls))]
     pool.starmap(check_url, jobs)
 
 output = {"url": [x for x in potential_urls if x is not None]}
-output['url'] = output['url'][0:1]
 print(json.dumps(output))
