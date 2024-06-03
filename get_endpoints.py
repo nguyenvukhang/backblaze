98c634272b272a7b1444ea1acaf73d30007cd927
nguyenvukhang <brew4k@gmail.com>
2024-06-03 09:43:26 +0800

    Get json output for valid urls

--- get_endpoints.py
+++ get_endpoints.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 from multiprocessing.pool import ThreadPool
-import requests, shutil
+import requests, shutil, json
 from os import path
-from tqdm import tqdm
 
 
 def dl_url(year: int, quarter: int):
@@ -21,26 +20,24 @@ def download_file(url) -> str:
 
 
 datetime.today()
-# download_file(dl_url(2024, 2))
-# for year in range(2016, datetime.today().year + 1):
-#     for quarter in range(1, 5):
-#         url = dl_url(year, quarter)
-#         with requests.get(url, stream=True) as r:
-#             if r.status_code == 200:
-#                 print(url)
-
-
-def print_url_if_valid(url: str):
-    with requests.get(url, stream=True) as r:
-        if r.status_code == 200:
-            print(url)
-
 
 potential_urls = []
 for year in range(2016, datetime.today().year + 1):
     for quarter in range(1, 5):
         potential_urls.append(dl_url(year, quarter))
 
+
+def check_url(urls: list[str], idx: int):
+    url = urls[idx]
+    with requests.get(url, stream=True) as r:
+        if r.status_code != 200:
+            urls[idx] = None
+
+
 with ThreadPool(4) as pool:
     total = len(potential_urls)
-    list(pool.map(print_url_if_valid, potential_urls))
+    jobs = [(potential_urls, i) for i in range(len(potential_urls))]
+    pool.starmap(check_url, jobs)
+
+output = {"urls": [x for x in potential_urls if x is not None]}
+print(json.dumps(output))
