1401073b64abda2823770563335f29866fcabd49
nguyenvukhang <brew4k@gmail.com>
2024-06-05 06:26:56 +0000

    Update downloader

--- downloader/index.mjs
+++ downloader/index.mjs
@@ -1,6 +1,10 @@
 import { Octokit } from 'octokit'
 import { spawnSync } from 'child_process'
-import { readFileSync } from 'fs'
+import { readFileSync, rmSync, statSync } from 'fs'
+
+const DOWNLOAD = true
+const token = loadToken()
+const gh = new Octokit({ auth: token })
 
 function loadToken() {
   try {
@@ -10,29 +14,37 @@ function loadToken() {
   }
 }
 
-const token = loadToken()
-const gh = new Octokit({ auth: token })
+/** @returns {string} */
+function buildCurl(asset, token) {
+  let cmd = `curl -o ${asset.name} -L -H "Accept: application/octet-stream"`
+  if (token) cmd += ` -H "Authorization: Bearer ${token}"`
+  return cmd + ` https://api.github.com/repos/nguyenvukhang/backblaze/releases/assets/${asset.id}`
+}
 
 async function main() {
   const rel = await gh.rest.repos.getReleaseByTag({
     owner: 'nguyenvukhang',
     repo: 'backblaze',
-    tag: 'v0.1',
+    tag: 'v0.2',
   })
-  // console.log(rel.data.assets)
+  console.log('Found', rel.data.assets.length, 'assets')
   for (let i = 0; i < rel.data.assets.length; i++) {
     const asset = rel.data.assets[i]
 
-    // spawnSync()
-    let cmd = `curl -o ${asset.name} -L`
-    cmd += ' -H "Accept: application/octet-stream"'
-    if (token) {
-      cmd += ` -H "Authorization: Bearer ${token}"`
+    let exists = false
+    let downloaded = false
+    try {
+      downloaded = statSync(asset.name).size === asset.size
+      exists = true
+    } catch {}
+
+    if (DOWNLOAD && !downloaded) {
+      rmSync(asset.name, { force: true })
+      console.log('[download]', asset.name, '...')
+      spawnSync(buildCurl(asset, token), { stdio: 'inherit', shell: true })
+    } else {
+      console.log('[skip]', asset.name)
     }
-    cmd += ` https://api.github.com/repos/nguyenvukhang/backblaze/releases/assets/${asset.id}`
-    console.log(`Downloading [${asset.name}] ...`)
-    console.log(cmd)
-    spawnSync(cmd, { stdio: 'inherit', shell: true })
   }
 }
 
