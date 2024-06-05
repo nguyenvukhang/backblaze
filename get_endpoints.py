2ebc04b0737083907d3f3d02e5d288e0f4e152dd
nguyenvukhang <brew4k@gmail.com>
2024-06-05 06:02:08 +0000

    Include 2013-2015

--- get_endpoints.py
+++ get_endpoints.py
@@ -8,7 +8,11 @@ def dl_url(year: int, quarter: int):
     return f"https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_Q{quarter}_{year}.zip"
 
 
-potential_urls = []
+potential_urls = [
+    "https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_2013.zip",
+    "https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_2014.zip",
+    "https://f001.backblazeb2.com/file/Backblaze-Hard-Drive-Data/data_2015.zip",
+]
 for year in range(2016, datetime.today().year + 1):
     for quarter in range(1, 5):
         potential_urls.append(dl_url(year, quarter))
