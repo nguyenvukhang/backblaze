770d0eafcfe40ec7646dbb1e60944fa63813bbac
nguyenvukhang <brew4k@gmail.com>
2024-06-26 20:21:11 +0800

    Inspection

--- migrate.py
+++ migrate.py
@@ -4,21 +4,30 @@ from zipfile import ZipFile
 import pandas as pd
 import pyarrow.parquet as pq
 import pyarrow as pa
-from multiprocessing.pool import ThreadPool
 from tqdm import tqdm
+from utils import *
+
+ded = lambda: exit(0)
 
 SRC_DIR = "/Users/khang/.local/data/backblaze/parquets"
 
 pq_files = os.listdir(SRC_DIR)
-pq_files.sort()
 pq_files = [x for x in pq_files if x.endswith(".parquet")]
+pq_files.sort()
+n = len(pq_files)
 
+start_date = datetime.strptime(pq_files[0].split(".")[0], "%Y-%m-%d")
 
-years = list({x.split("-")[0] for x in pq_files})
-years.sort()
+# check that all files are in order.
+for t, pq_file in zip(day_iter_n(start_date, n), pq_files):
+    assert t.strftime("%Y-%m-%d") == pq_file.split(".")[0]
 
 for pq_file in tqdm(pq_files):
-    df = pq.read_table(path.join(SRC_DIR, pq_file)).to_pandas().reset_index()
+    df = read_pandas(path.join(SRC_DIR, pq_file))
+    if len(df.index) < 10000:
+        print(df["model"].unique())
+        ded()
+    # print(len(df))
     # print(df)
     # break
-    pq.write_table(pa.Table.from_pandas(df), path.join("output", pq_file))
+    # pq.write_table(pa.Table.from_pandas(df), path.join("output", pq_file))
---
+++ utils.py
@@ -0,0 +1,66 @@
+import numpy as np, pyarrow.parquet as pq, pyarrow as pa
+from pandas import DataFrame
+from tqdm import tqdm
+from datetime import datetime, timedelta
+from typing import Iterable
+from dateutil import tz
+
+
+def read_pandas(path: str) -> DataFrame:
+    return pq.read_table(path).to_pandas()
+
+
+def write_pandas(df: DataFrame, path: str):
+    pq.write_table(pa.Table.from_pandas(df), path)
+
+
+def unsqueeze(x: np.ndarray) -> np.ndarray:
+    return np.expand_dims(x, axis=0)
+
+
+def std_date(date: datetime) -> str:
+    """
+    Uses `%Y-%m-%d` to format the date. Standard project-wide.
+    """
+    return date.strftime("%Y-%m-%d")
+
+
+def day_iter(start: datetime, end: datetime) -> Iterable[datetime]:
+    """
+    An (inclusive) iterator from `start` to `end`, incrementing in days.
+    """
+    day = timedelta(days=1)
+    while start <= end:
+        yield start
+        start += day
+
+
+def day_iter_tqdm(start: datetime, end: datetime) -> Iterable[datetime]:
+    """
+    An (inclusive) iterator from `start` to `end`, incrementing in days.
+    """
+    day = timedelta(days=1)
+    bar = tqdm(total=(end - start).days + 1)
+    while start <= end:
+        bar.set_description(std_date(start))
+        yield start
+        bar.update()
+        start += day
+    bar.close()
+
+
+def day_iter_n(start: datetime, n: int) -> Iterable[datetime]:
+    """
+    An iterator of length `n`, starting from `start`.
+    """
+    day = timedelta(days=1)
+    for _ in range(n):
+        yield start
+        start += day
+
+
+def today() -> datetime:
+    """
+    Get today's datetime (Singapore TZ)
+    """
+    return datetime.now(tz.gettz("Asia/Singapore"))
