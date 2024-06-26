9965e7d92f350d5b91bffce392802a587e5c9571
nguyenvukhang <brew4k@gmail.com>
2024-06-27 07:22:11 +0800

    Try to read the artifacts directly

--- collate.py
+++ collate.py
@@ -1,6 +1,21 @@
 from sys import argv
 import json
+from utils import *
+import pandas as pd
+
+# TODO: the artifacts are stored in the directories that are named `asset['id']`
+# Iterate over these directories.
 
 data = json.loads(" ".join(argv[1:]))
 print(type(data))
 print(data)
+
+dfs = []
+for a in data["include"]:
+    df = read_pandas(a["id"])
+    dfs.append(df)
+    print(df)
+    print('------------')
+
+df = pd.concat(dfs)
+print(df)
