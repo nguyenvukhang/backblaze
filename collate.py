ff17e44edb253bd33707d621cddf14b0723e34a4
nguyenvukhang <brew4k@gmail.com>
2024-06-27 09:23:16 +0800

    Read each directory

--- collate.py
+++ collate.py
@@ -2,6 +2,7 @@ from sys import argv
 import json
 from utils import *
 import pandas as pd
+from os import listdir, path
 
 # TODO: the artifacts are stored in the directories that are named `asset['id']`
 # Iterate over these directories.
@@ -10,12 +11,17 @@ data = json.loads(" ".join(argv[1:]))
 print(type(data))
 print(data)
 
-dfs = []
+dfs = {}
 for a in data["include"]:
-    df = read_pandas(a["id"])
-    dfs.append(df)
-    print(df)
-    print('------------')
+    for pq_file in listdir(a["id"]):
+        pq_file = path.join(a["id"], pq_file)
+        df = read_pandas(pq_file)
+        if a["name"] in dfs:
+            dfs[a["name"]].append(df)
+        else:
+            dfs[a["name"]] = [df]
 
-df = pd.concat(dfs)
-print(df)
+for name, dfs in dfs.items():
+    print("----------------")
+    print("|", name, "|")
+    print(pd.concat(dfs))
