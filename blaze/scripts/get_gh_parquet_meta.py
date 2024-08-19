4d62225c8a83e0fc5a373cad5bc8cb30fc105ec8
nguyenvukhang <brew4k@gmail.com>
2024-08-19 21:40:37 +0800

    Try build

---
+++ blaze/scripts/get_gh_parquet_meta.py
@@ -0,0 +1,19 @@
+# This script fetches all the metadata from the verbatim copy hosted on GitHub
+# Actions. This includes getting the most up-to-date list of parquets.
+#
+# This script is used on GitHub Actions in the postprocessing stage.
+
+import requests, json, os
+
+TAG = "v1.0"
+
+BASE_URL = "https://api.github.com/repos/nguyenvukhang/backblaze"
+api_github = lambda v: os.path.join(BASE_URL, v)
+
+
+HEADERS = { "Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28" }  # fmt: skip
+os.makedirs("output", exist_ok=True)
+res = requests.get(api_github(f"releases/tags/{TAG}"), headers=HEADERS).json()
+assets = filter(lambda a: a["name"].endswith(".zip"), res["assets"])
+incl = [{"id": str(asset["id"]), "name": str(asset["name"])} for asset in assets]
+print(json.dumps({"include": incl}))
---
+++ blaze/scripts/gh_derivative_build.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+TOKEN, ASSET_ID, ASSET_NAME = argv[1:4]
+
+print(TOKEN, ASSET_ID, ASSET_NAME)
