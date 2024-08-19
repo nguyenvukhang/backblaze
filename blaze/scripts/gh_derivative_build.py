724e58070c3f605239732236b7c449950dc75c3e
nguyenvukhang <brew4k@gmail.com>
2024-08-19 21:46:22 +0800

    Generate derivate builds

--- blaze/scripts/gh_derivative_build.py
+++ blaze/scripts/gh_derivative_build.py
@@ -1,5 +1,33 @@
 from sys import argv
+import os, subprocess, pandas as pd
+from typing import cast
+from tqdm import tqdm
+from zipfile import ZipFile
+
+from blaze.utils import *
 
 TOKEN, ASSET_ID, ASSET_NAME = argv[1:4]
 
-print(TOKEN, ASSET_ID, ASSET_NAME)
+BASE_URL = "https://api.github.com/repos/nguyenvukhang/backblaze"
+api_github = lambda v: os.path.join(BASE_URL, v)
+HEADERS = { "Accept": "application/vnd.github+json", "X-GitHub-Api-Version": "2022-11-28" }  # fmt: skip
+
+
+def curl(id: str, target: str):
+    cmd = ["curl", "-o", target, "-L", "-H", "Accept: application/octet-stream"]
+    cmd += ["-H", f"Authorization: Bearer {TOKEN}"] if TOKEN is not None else []
+    cmd += [api_github(f"releases/assets/{id}")]
+    subprocess.run(cmd)
+
+
+curl(id=ASSET_ID, target=ASSET_NAME)
+
+with ZipFile(ASSET_NAME, "r") as z:
+    DF = cast(DataFrame, None)
+    for member in tqdm(z.namelist()):
+        df = bytes_to_dataframe(z.read(member))
+        date_str = file_stem(member)
+
+        df = df[df["model"] == "ST4000DM000"]
+        DF = df if DF is None else pd.concat((DF, df))
+    write_pandas(DF, file_stem(ASSET_NAME) + ".ST4000DM000.parquet")
--- blaze/utils.py
+++ blaze/utils.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from os import path
 from pandas import DataFrame
-import pyarrow.parquet as pq
+import pyarrow.parquet as pq, pyarrow as pa
 
 
 def file_stem(x: str) -> str:
@@ -11,3 +11,11 @@ def file_stem(x: str) -> str:
 def bytes_to_dataframe(b: bytes) -> DataFrame:
     "Parquet Table bytes to a Pandas DataFrame."
     return pq.read_table(BytesIO(b)).to_pandas()
+
+
+def write_pandas(df: DataFrame, path: str):
+    pq.write_table(pa.Table.from_pandas(df), path)
+
+
+def read_pandas(path: str) -> DataFrame:
+    return pq.read_table(path).to_pandas()
